6927ba9cb505ea238a1987dd018a98d823d96674
Valentin Brosseau <c4software@gmail.com>
2017-03-31 21:52:40 +0200

    Update Info

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ OPTIONS = {
         'LSUIElement': True,
         'CFBundleName': APP_NAME,
         'CFBundleDisplayName': APP_NAME,
-        'CFBundleGetInfoString': "Making Sandwiches",
+        'CFBundleGetInfoString': "Create Menu",
         'CFBundleIdentifier': "com.vbrosseau.menubar-quick-access",
         'CFBundleVersion': "0.1.0",
         'CFBundleShortVersionString': "0.1.0",
